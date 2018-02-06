810abbb39011e1d187f2e695ccf7f0948b2b36e7
YS <yogendrasingh890@gmail.com>
2018-02-06 23:04:20 +0530

    improvements.

--- app/src/main/java/app/ys/quakeview/Splash.java
+++ app/src/main/java/app/ys/quakeview/Splash.java
@@ -4,13 +4,15 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 
 
 /**
  * Created by YS on 12-12-2017.
  */
 
-public class Splash extends Activity {
+public class Splash extends AppCompatActivity {
 
 
         private static final int SPLASH_DISPLAY_TIME = 4000; // splash screen delay time
@@ -19,6 +21,8 @@ public class Splash extends Activity {
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.splash);
+            ActionBar myActionBar = getSupportActionBar();
+            myActionBar.hide();
 
             new Handler().postDelayed(new Runnable() {
                 public void run() {
