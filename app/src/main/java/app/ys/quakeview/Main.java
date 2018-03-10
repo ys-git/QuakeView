d3e936cfbcfb11c222837ca05a66daacc306949b
YS <yogendrasingh890@gmail.com>
2018-03-10 19:43:06 +0530

    minor changes.

--- app/src/main/java/app/ys/quakeview/Main.java
+++ app/src/main/java/app/ys/quakeview/Main.java
@@ -8,6 +8,10 @@ import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 import org.xmlpull.v1.XmlPullParserFactory;
@@ -42,6 +46,15 @@ public class Main extends AppCompatActivity {
             } catch (XmlPullParserException e) {
                 e.printStackTrace();
             }
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
 
 
             BackgroundAsyncTask backgroundAsyncTask = new BackgroundAsyncTask();
